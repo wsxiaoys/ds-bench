2f1159a01e11326a68bc3a66be8bd3bca8d11b7e
Wei Zhang <kweizh@tabbyml.com>
2026-06-26 01:27:03 +0800

    fix(task): always install uv in docker if test.sh used (#33)

--- tasks/e2b_python_env_setup_uv/tests/test.sh
+++ tasks/e2b_python_env_setup_uv/tests/test.sh
@@ -3,10 +3,7 @@
 # Use this file to install test dependencies and run the tests.
 # It will be copied to /tests/test.sh and run from the working directory.
 
-apt-get update
-apt-get install -y curl
 
-curl -LsSf https://astral.sh/uv/0.9.7/install.sh | sh
 
 source $HOME/.local/bin/env
 
--- tasks/tanstack_query_products/tests/test.sh
+++ tasks/tanstack_query_products/tests/test.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-
-
 pytest --ctrf /logs/verifier/ctrf.json /tests/test_final_state.py -rA
 
 if [ $? -eq 0 ]; then
