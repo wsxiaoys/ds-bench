f7d3e6ac399db807d0acd54521dc806db9ea24af
Wei Zhang <kweizh@tabbyml.com>
2026-08-18 19:20:15 +0800

    chore(jobs): clean up duplicated jobs

--- jobs/2026-08-01__21-10-52/wasp_file_management_system__Gi6nC6n/artifacts/home/user/app/src/pages/FolderPage.tsx
+++
@@ -1,7 +0,0 @@
-import { useParams } from "react-router";
-import { Dashboard } from "../components/Dashboard";
-
-export function FolderPage() {
-  const { folderId } = useParams();
-  return <Dashboard folderId={folderId} />;
-}
